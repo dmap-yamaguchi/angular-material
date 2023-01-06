a6a27b415c4c0a636c79c4fc3331a389768736ef
dmap-yamaguchi <yamaguchi.shunya@dmap.jp>
2023-01-06 16:46:46 +0900

    const追加

---
+++ src/app/app-const.ts
@@ -0,0 +1,8 @@
+export class AppConst {
+    //
+
+    // APIリターンコード
+    static readonly RET_SUCCESS = 0;              // 0: 正常終了
+    static readonly RET_INVALID_INPUT = 1;        // 1: 入力引数不正
+    static readonly RET_SERVER_ERROR = 100;       // 100: サーバ処理エラー
+}
